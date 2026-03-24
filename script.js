b7fcf5fa54c5e4f1ffe638f60e8a7d299b5e68c4
sara luongo <saraluongo202002@gmail.com>
2026-03-24 17:14:39 +0100

    modifica: al posto del 101 100 con segno <=

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 //ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-//Creamo un ciclo che stampi numeri da 1 a 100.
-for (let i = 1; i < 101; i++) {
+//Creiamo un ciclo che stampi numeri da 1 a 100.
+for (let i = 1; i <= 100; i++) {
 
     //calcolo con operatore di modulo tutti i multipli di 3 e di 5
     if (i % 3 === 0 && i % 5 === 0) {
