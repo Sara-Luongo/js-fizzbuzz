009e7bc6afb017d770150d39a2fc6ea5617ba6b9
sara luongo <saraluongo202002@gmail.com>
2026-03-24 16:54:29 +0100

    terminato esercizio dove ho fatto da riga 10 a 12 il calcolo per entrambi i multipli da riga 15 a 17 calcolo per i multipli solo del 3 e dalla 20 alla 21 per i multipli del 5 dalla 25 alla 27 un else per stampare gli altri numeri non multipli

--- script.js
+++ script.js
@@ -4,3 +4,27 @@
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 //Creamo un ciclo che stampi numeri da 1 a 100.
+for (let i = 1; i < 101; i++) {
+
+    //calcolo con operatore di modulo tutti i multipli di 3 e di 5
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz')
+    }
+
+    //calcolo con operatore di modulo tutti i multipli di 3
+    else if (i % 3 === 0) {
+        console.log('fizz')
+    }
+
+    //calcolo con operatore di modulo tutti i multipli di 5
+    else if (i % 5 === 0) {
+        console.log('Buzz')
+    }
+
+    //tutti gli atri numeri non multipli ne di 3 ne di 5 verranno stampati normalmente
+    else {
+        console.log(i)
+    }
+}
+
+
